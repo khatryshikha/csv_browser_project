4a0ab88071bbb9951c037eee338e776511752cdb
khatryshikha <shikhakhatry2612@gmail.com>
2018-08-22 16:23:02 +0530

    added data integration functionality

--- schlum/settings.py
+++ schlum/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -40,11 +38,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -63,7 +59,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -76,31 +71,13 @@ WSGI_APPLICATION = 'schlum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+
 
 
 # Internationalization
--- wines/views.py
+++ wines/views.py
@@ -2,23 +2,25 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from database import db
+import csv
+import time
 # Create your views here.
 
 def check_new_entry(request):
     if request.method == 'GET':
         return render(request, 'upload_new_winedata.html')
-    # elif request.method == 'POST':
-    #     winery = request.POST.get('winery')
-    #     country = request.POST.get('country')
-    #     province = request.POST.get('province')
-    #     region1 = request.POST.get('region1')
-    #     region2 = request.POST.get('region2')
-    #     variety = request.POST.get('variety')
-    #     price = request.POST.get('price')
-    #     points = request.POST.get('point')
-    #     description = request.POST.get('description')
-    #     designation = request.POST.get('designation')
-    #     db.wines.insert({'winery' : winery, 'country' : country , 'province' : province, 'region1' : region1, 'region2' : region2, 'variety': variety, 'price' : price, 'point' : point , 'description' : description, 'designation' : designation})
-
-
+    elif request.method == 'POST':
+        files = request.FILES['datafile']
+        spamreader = csv.reader(files, delimiter=str(','))
+        count = 0
+        data = []
+        head_list = []
+        for row in spamreader:
+            if ( count == 0 ):
+                head_list = row
+                count = count + 1
+            else: 
+                data.append(dict(zip(head_list, row)))
+        db.test.insert(data)
+        return render(request, 'index.html')
